1a07c0553c203c7b38e066641312d199e06730bf
piyalkarum <piyalkarumail@yahoo.com>
2022-05-03 15:58:46 +0200

    mods

--- R/ratios.R
+++ R/ratios.R
@@ -276,68 +276,14 @@ model.signif<-function(data){
   } else {
     message("Only one strain present; check your data")
   }
+  cat("Output details
+  model.pval - significance value of ANOVA on the binomial GLM test of the strain pair
+  bonferroni - significance of the model.pval with bonferroni correction
+  res.Dv - residual deviance
+  thr - threshold for the significance of the pvalue
+  str - values for the strains
+  int - values for the interaction between the strain and the dose
+          ")
   return(list(model=Test))
 }
 
-
-
-
-
-model.signif1<-function(data){
-  data$strain<-as.factor(data$strain)
-  strains<-levels(data$strain)
-  if (length(strains)>=2) {
-    Test<-reg.pair(data)
-    if(length(strains)>2 & Test$pairT$`Pr(>Chi)`[2]>0.05){
-      message("effect on strains are non-significant \n all strains come from the same population")
-    } else if(length(strains)>2 & Test$pairT$`Pr(>Chi)`[2]<=0.05){
-      print(Test$pairT)
-      message("complete model is significant against a NULL model \n continueing to pair-wise comparison")
-      Test<-sapply(strains, function(x,data) sapply(strains, function(y,data){
-        if(x!=y){
-          dat<-data[data$strain==x | data$strain==y,]
-          reg.pair(dat)$pairT$Pr[2]
-        }
-      },data=data),data=data)
-      dv<-sapply(strains, function(x,data){
-        sapply(strains, function(y,data){
-          if(x!=y){
-            dat<-data[data$strain==x | data$strain==y,]
-            reg.pair(dat)$pairT$Deviance[2]
-          }
-        },data=data)
-      },data=data)
-
-      rdl<-combn(strains,2,function(x,data){
-        dat<-data[data$strain==x[1] | data$strain==x[2],]
-        tmp<-reg.pair(dat)$fullM
-        list(c(round(tmp$`Resid. Dev`[1],2),round(as.numeric(tmp$`Resid. Dev`[3:4]),3),round(as.numeric(tmp$`Pr(>F)`[3:4]),3)))
-      },data=data)
-      rdl<-do.call(rbind,rdl)
-
-      rk<-(length(strains)*(length(strains)-1)/2):1
-      rk<-0.05/rk
-      toget<-t(combn(rownames(Test),2))
-      pval<-unlist(Test[toget])
-      Test<-data.frame(cbind(toget,round(unlist(Test[toget]),5),
-                             ifelse(pval<rk,"sig","non-sig")))
-      rdl[which(Test[,3]>0.05),]<-NA
-
-      ### bonferr for pvals
-      bp<-cbind(1:(nrow(Test)*2),as.numeric(unlist(rdl[,4:5])))
-      bp<-bp[order(unlist(bp[,2])),]
-      bp0<-length(na.omit(unlist(bp[,2])))
-      th<-unlist(lapply(1:bp0,function(x){0.05/(bp0-x+1)}))
-      tmp<-suppressWarnings(cbind(bp,th))
-      tmp[which(is.na(tmp[,2])),3]<-NA
-      tmp<-round(tmp[order(tmp[,1]),3],5)
-      tmp<-split(tmp,cut(seq_along(tmp),2,labels = F))
-
-      Test<-cbind(Test,rdl[,1:3],rdl[,4],tmp$`1`,rdl[,5],tmp$`2`)
-      colnames(Test)<-c("strain1","strain2","model.pval","bonferroni","res.Dv.Null","res.Dv.str","res.Dv.int","str.pval","str.thr","int.pval","int.thr")
-    }
-  } else {
-    message("Only one strain present; check your data")
-  }
-  return(list(model=Test))
-}
